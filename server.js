c04ce27814010d0dd97f5f9faf40289f39c57c8b
Niloufar Sabbaghian <niloufarsabbaghian@Niloufars-iMac.local>
2022-11-01 14:01:15 -0700

    notes.html working

--- server.js
+++ server.js
@@ -2,27 +2,28 @@ const express = require('express');
 const path = require ('path');
 const apiRouter = require('./routes/apiRouter');
 
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 // mount the appiRouter onto the express app - anytime you see the enpoint /api go get the apiRouter
 app.use('/api', apiRouter);
 
-app.use(express.static('public'));
+// Get route for notes html page
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // Route for home page html (or any wildcard *)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-// Get route for notes html page
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 app.listen(PORT, () => 
     console.log(`App now running on http://localhost:${PORT}`)
